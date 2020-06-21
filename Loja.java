b2beeb1d8751a4efc9662fe82fdb3b6a8e5f5654
wildevn <45374307+wildevn@users.noreply.github.com>
2020-06-21 17:23:39 -0300

    Create Loja.java

---
+++ Loja.java
@@ -0,0 +1,35 @@
+class Loja{
+
+  private String idLoja, nome, endereco;
+  //private String[] clientes = new String[100];
+  private Produto[] produtos = new Produto[]
+
+  // Construtor
+  Loja(String idLoja, String nome, String endereco){
+    setIdLoja(idLoja);
+    setNome(nome);
+    setEndereco(endereco);
+  }
+
+  // Setters e getters
+  void setIdLoja(String idLoja){
+    this.idLoja = idLoja;
+  }
+  void setNome(String nome){
+    this.nome = nome;
+  }
+  void setEndereco(String endereco){
+    this.endereco = endereco;
+  }
+  String getIdLoja(){
+    return idLoja;
+  }
+  String getNome(){
+    return nome;
+  }
+  String getEndereco(){
+    return endereco;
+  }
+
+
+}
