a7327032552a5e483cb37b87101b3f9b2300fd3e
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-15 13:46:33 -0300

    Update Janela.java

--- Janela.java
+++ Janela.java
@@ -1,15 +1,20 @@
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class Janela
 {
 private JFrame janela;
-private JMenuBar barraDeMenu;
 
-private MenuQualquer menuListarLojas;
+private JMenuBar barraDeMenu;
+private JLabel barraDeStatus;
+
+
+private JButton botaoListarLojas;
 private MenuQualquer menuBuscaProduto;
 private MenuQualquer menuCadastrar;
 
@@ -17,44 +22,68 @@ private MenuQualquer menuCadastrar;
     Janela(String titulo)
     {
         janela = new JFrame(titulo);
+        janela.setLayout(new BorderLayout());
 
+        barraDeStatus = new JLabel(" ");
         barraDeMenu = new JMenuBar();
-    
+        
         configMenuBuscaProduto();
-        configMenuListarLojas();
+        configBotaoListarLojas();
         configMenuCadastrar();
 
         barraDeMenu.add(menuBuscaProduto.getMenu());
-        barraDeMenu.add(menuListarLojas.getMenu());
+        barraDeMenu.add(botaoListarLojas);
         barraDeMenu.add(menuCadastrar.getMenu());
 
+        janela.add(barraDeStatus, BorderLayout.SOUTH);
         janela.setJMenuBar(barraDeMenu);
         janela.setSize(800, 600);
         janela.setVisible(true);
     }
 
 // métodos
-public void configMenuListarLojas()
+public void configBotaoListarLojas()
 {
-    menuListarLojas = new MenuQualquer("Buscar", 0);
-    // menuBuscar.setOpcaoNova("Lojas");
-    // setados os nomes e objetos, porem ainda sem action listener
+OuvidoDoBotaoListarLojas ouvidoDoBotaoListarLojas = new OuvidoDoBotaoListarLojas(barraDeStatus);
+
+    // config nome
+    botaoListarLojas = new JButton("Lista de Lojas");
+    // action listener
+    botaoListarLojas.addActionListener(ouvidoDoBotaoListarLojas);
 }
 public void configMenuBuscaProduto()
 { 
+OuvidoDoBotaoBuscaTodosProdutos ouvidoDoBotaoBuscaTodosProdutos = new OuvidoDoBotaoBuscaTodosProdutos(barraDeStatus);
+OuvidoDoBotaoBuscaAcessorios ouvidoDoBotaoBuscaAcessorios = new OuvidoDoBotaoBuscaAcessorios(barraDeStatus);
+OuvidoDoBotaoBuscaDiaADia ouvidoDoBotaoBuscaDiaADia = new OuvidoDoBotaoBuscaDiaADia(barraDeStatus);
+OuvidoDoBotaoBuscaTecnologia ouvidoDoBotaoBuscaTecnologia = new OuvidoDoBotaoBuscaTecnologia(barraDeStatus);
+
+    // config nomes do menu e das opcoes
     menuBuscaProduto = new MenuQualquer("Produtos", 4);
-    menuBuscaProduto.setOpcaoNova("Todos os Produtos");
-    menuBuscaProduto.setOpcaoNova("Acessorios");
+    menuBuscaProduto.setOpcaoNova("Todos os Produtos"); 
+    menuBuscaProduto.setOpcaoNova("Acessorios"); 
     menuBuscaProduto.setOpcaoNova("Dia a Dia");
     menuBuscaProduto.setOpcaoNova("Tecnologia");
-    // setados os nomes e objetos, porem ainda sem action listener
+    // action listeners
+    menuBuscaProduto.adicionaActionListener(ouvidoDoBotaoBuscaTodosProdutos, 0);
+    menuBuscaProduto.adicionaActionListener(ouvidoDoBotaoBuscaAcessorios, 1);
+    menuBuscaProduto.adicionaActionListener(ouvidoDoBotaoBuscaDiaADia, 2);
+    menuBuscaProduto.adicionaActionListener(ouvidoDoBotaoBuscaTecnologia, 3);
 }
 public void configMenuCadastrar()
 {
+OuvidoDoBotaoCliente ouvidoDoBotaoCliente = new OuvidoDoBotaoCliente(barraDeStatus);
+OuvidoDoBotaoCadastrarLoja ouvidoDoBotaoCadastrarLoja = new OuvidoDoBotaoCadastrarLoja(barraDeStatus);
+OuvidoDoBotaoCadastrarProduto ouvidoDoBotaoCadastrarProduto = new OuvidoDoBotaoCadastrarProduto(barraDeStatus);
+
+    // config nome do menu e das opcoes
     menuCadastrar = new MenuQualquer("Cadastrar", 3);
     menuCadastrar.setOpcaoNova("Cliente");
     menuCadastrar.setOpcaoNova("Loja");
     menuCadastrar.setOpcaoNova("Produto");
-    // setados os nomes e objetos, porem ainda sem action listener
+    // action listeners
+    menuCadastrar.adicionaActionListener(ouvidoDoBotaoCliente, 0);
+    menuCadastrar.adicionaActionListener(ouvidoDoBotaoCadastrarLoja, 1);
+    menuCadastrar.adicionaActionListener(ouvidoDoBotaoCadastrarProduto, 2);
 }
 }
