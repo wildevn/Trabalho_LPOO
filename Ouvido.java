094d3333e970fd4004968e1d60a54a62b66bb0fa
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-17 03:27:20 -0400

    Update Ouvido.java

--- Ouvido.java
+++ Ouvido.java
@@ -2,19 +2,19 @@ import javax.swing.JLabel;
 
 public class Ouvido
 {
-protected JLabel barraDeStatus;
+    protected JLabel barraDeStatus;
 
     Ouvido(JLabel barraDeStatus)
     {
         setBarraDeStatus(barraDeStatus);
     }
 
-public void setBarraDeStatus(JLabel barraDeStatus)
-{
-    this.barraDeStatus = barraDeStatus;
-}
-public JLabel getBarraDeStatus()
-{
-    return this.barraDeStatus;
-}
+    public void setBarraDeStatus(JLabel barraDeStatus)
+    {
+        this.barraDeStatus = barraDeStatus;
+    }
+    public JLabel getBarraDeStatus()
+    {
+        return this.barraDeStatus;
+    }
 }
