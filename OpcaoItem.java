0b29ac6c96d39a95f6e694f78427eecfbcc74dfb
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-18 15:25:41 -0400

    Update OpcaoItem.java

--- OpcaoItem.java
+++ OpcaoItem.java
@@ -2,28 +2,28 @@ import javax.swing.JMenuItem;
 
 public class OpcaoItem extends Opcao
 {
-private JMenuItem item;
+    private JMenuItem item;
 
     // Construtores
-    OpcaoItem(JMenuItem item, String nomeItem)
-    {
-        if(nomeItem == null)
-            setOpcaoPronta(item); //  item ja criado
-        else
-            setOpcaoNova(nomeItem); // criando novo item
-    }
+        OpcaoItem(JMenuItem item, String nomeItem)
+        {
+            if(nomeItem == null)
+                setOpcaoPronta(item); //  item ja criado
+            else
+                setOpcaoNova(nomeItem); // criando novo item
+        }
 
-// Setters e Getters
-public void setOpcaoPronta(JMenuItem item)
-{
-    this.item = item;
-}
-public void setOpcaoNova(String nomeItem)
-{
-    this.item = new JMenuItem(nomeItem);
-}
-public JMenuItem getOpcaoX()
-{
-    return this.item;
-}
+    // Setters e Getters
+    public void setOpcaoPronta(JMenuItem item)
+    {
+        this.item = item;
+    }
+    public void setOpcaoNova(String nomeItem)
+    {
+        this.item = new JMenuItem(nomeItem);
+    }
+    public JMenuItem getOpcaoX()
+    {
+        return this.item;
+    }
 }
