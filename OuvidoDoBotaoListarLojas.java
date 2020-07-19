647a10609058d8f406207818a3355b5950728e07
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-19 17:05:22 -0300

    Update OuvidoDoBotaoListarLojas.java

--- OuvidoDoBotaoListarLojas.java
+++ OuvidoDoBotaoListarLojas.java
@@ -19,38 +19,39 @@ public class OuvidoDoBotaoListarLojas implements ActionListener
         setJanela(janela);
     }
 
-@Override
-public void actionPerformed(ActionEvent evento)
-{
-    JLabel x;
-    JPanel painelListarLojas;
-    janela.getBarraDeStatus().setText("carregando Listar Lojas...");
-    // encontrar e puxar todas as lojas do BD e a quantidade
-
-    painelListarLojas = new JPanel(new GridLayout(20, 5)); // grid deve ser no formato (nLojas, 5 colunas)
-
-    for(int i = 0; i < 100; i++)
+    @Override
+    public void actionPerformed(ActionEvent evento)
     {
-        if((i % 5) == 0)
-            x = new JLabel("nomeProduto");
-        else if((i % 5) == 1)
-            x = new JLabel("");
-        else if((i % 5) == 2)
-            x = new JLabel("quantidade: x");
-        else if((i % 5) == 3)
-            x = new JLabel("");
-        else
-            x = new JLabel("RS w.00");
-        painelListarLojas.add(x);
+        JLabel aux;
+        JPanel painelListarLojas;
+        janela.getBarraDeStatus().setText("carregando Listar Lojas...");
+        // encontrar e puxar todas as lojas do BD e a quantidade
+
+        painelListarLojas = new JPanel(new GridLayout(20, 5)); // grid deve ser no formato (nLojas, 5 colunas)
+
+        for(int i = 0; i < 100; i++)
+        {
+            if((i % 5) == 0)
+                aux = new JLabel("nomeProduto");
+            else if((i % 5) == 1)
+                aux = new JLabel("");
+            else if((i % 5) == 2)
+                aux = new JLabel("quantidade: x");
+            else if((i % 5) == 3)
+                aux = new JLabel("");
+            else
+                aux = new JLabel("RS w.00");
+            painelListarLojas.add(aux);
+        }
+        janela.removePainelAnterior();
+        janela.setPainelAnterior(painelListarLojas);
+        janela.getFrame().add(painelListarLojas, BorderLayout.CENTER);
+        janela.getBarraDeStatus().setText(" ");
+        janela.getFrame().setVisible(true);
     }
-    
-    janela.getFrame().add(painelListarLojas, BorderLayout.CENTER);
-    janela.getBarraDeStatus().setText(" ");
-    janela.getFrame().setVisible(true);
-}
-public void setJanela(Janela janela)
-{
-    this.janela = janela;
-}
+    public void setJanela(Janela janela)
+    {
+        this.janela = janela;
+    }
 
 }
