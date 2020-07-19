931078be901c438dd73e372cde4d7c005c571067
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-19 11:45:45 -0300

    Add files via upload

--- Ouvido.java
+++ Ouvido.java
@@ -1,20 +1,4 @@
-import javax.swing.JLabel;
-
 public class Ouvido
 {
-    protected JLabel barraDeStatus;
-
-    Ouvido(JLabel barraDeStatus)
-    {
-        setBarraDeStatus(barraDeStatus);
-    }
-
-    public void setBarraDeStatus(JLabel barraDeStatus)
-    {
-        this.barraDeStatus = barraDeStatus;
-    }
-    public JLabel getBarraDeStatus()
-    {
-        return this.barraDeStatus;
-    }
+    xyz;
 }
