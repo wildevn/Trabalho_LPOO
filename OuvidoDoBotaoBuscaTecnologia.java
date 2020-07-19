63b56b95873ff744344705e506c0ef6e3451baf1
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-19 17:25:06 -0300

    Update OuvidoDoBotaoBuscaTecnologia.java

--- OuvidoDoBotaoBuscaTecnologia.java
+++ OuvidoDoBotaoBuscaTecnologia.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,14 +18,40 @@ public class OuvidoDoBotaoBuscaTecnologia implements ActionListener
         setJanela(janela);
     }
 
-@Override
-public void actionPerformed(ActionEvent evento)
-{
-    janela.getBarraDeStatus().setText("testei buscar Tecnologia");
+    @Override
+    public void actionPerformed(ActionEvent evento)
+    {
+    JLabel aux;
+    JPanel painelTecnologia;
+
+        janela.getBarraDeStatus().setText("carregando Tecnologia...");
+        // encontrar e puxar todas as lojas do BD e a quantidade
+
+        painelTecnologia = new JPanel(new GridLayout(20, 5)); // grid deve ser no formato (nLojas, 5 colunas)
+
+        for(int i = 0; i < 100; i++)
+        {
+            if((i % 5) == 0)
+                aux = new JLabel("nomeProdutoTecnologico");
+            else if((i % 5) == 1)
+                aux = new JLabel("");
+            else if((i % 5) == 2)
+                aux = new JLabel("quantidade: x");
+            else if((i % 5) == 3)
+                aux = new JLabel("");
+            else
+                aux = new JLabel("RS w.00");
+            painelTecnologia.add(aux);
+        }
+        janela.removePainelAnterior();
+        janela.setPainelAnterior(painelTecnologia);
+        janela.getFrame().add(painelTecnologia, BorderLayout.CENTER);
+        janela.getBarraDeStatus().setText(" ");
+        janela.getFrame().setVisible(true);
 }
 
-public void setJanela(Janela janela)
-{
-    this.janela = janela;
-}
+    public void setJanela(Janela janela)
+    {
+        this.janela = janela;
+    }
 }
