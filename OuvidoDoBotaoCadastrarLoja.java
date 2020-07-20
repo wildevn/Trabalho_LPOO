88fc8ce5104ab5c0d4e37cb8e7ea40f44ad5b371
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-20 18:09:43 -0300

    Update OuvidoDoBotaoCadastrarLoja.java

--- OuvidoDoBotaoCadastrarLoja.java
+++ OuvidoDoBotaoCadastrarLoja.java
@@ -2,7 +2,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
+import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,6 +14,8 @@ import javax.swing.JPanel;
 public class OuvidoDoBotaoCadastrarLoja implements ActionListener
 {
     private Janela janela;
+    JTextField txtNomeLoja;
+    JTextField txtEnderecoLoja;
     OuvidoDoBotaoCadastrarLoja(Janela janela)
     {
         setJanela(janela);
@@ -20,7 +24,28 @@ public class OuvidoDoBotaoCadastrarLoja implements ActionListener
 @Override
 public void actionPerformed(ActionEvent evento)
 {
-    janela.getBarraDeStatus().setText("testei cadastrar lojas");
+    JButton botaoOkLoja = new JButton("Cadastrar");
+    JPanel painelCadastramento = new JPanel(new GridLayout(4, 2));
+    janela.getBarraDeStatus().setText("carregando cadastramento de loja...");
+
+    // itens da loja
+    painelCadastramento.add(new JLabel("Nome da Loja: "));
+    txtNomeLoja = new JTextField();
+    painelCadastramento.add(txtNomeLoja);
+
+    painelCadastramento.add(new JLabel("Endereco: "));
+    txtEnderecoLoja = new JTextField();
+    painelCadastramento.add(txtEnderecoLoja);
+
+    botaoOkLoja.addActionListener(new OuvidoDoBotaoOkLoja(janela, txtNomeLoja, txtEnderecoLoja));
+    painelCadastramento.add(new JLabel(""));
+    painelCadastramento.add(botaoOkLoja);
+    janela.getBarraDeStatus().setText(" ");
+
+    janela.removePainelAnterior();
+    janela.setPainelAnterior(painelCadastramento);
+    janela.getFrame().add(painelCadastramento);
+    janela.getFrame().setVisible(true);
 }
 
 public void setJanela(Janela janela)
