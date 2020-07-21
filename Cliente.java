c977e397848c668c12c798d38457fab02ac985f1
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-21 01:15:40 -0400

    Update Cliente.java

--- Cliente.java
+++ Cliente.java
@@ -1,53 +1,66 @@
 class Cliente{
 
-  private String idCliente, nome, cpf, endereco, numCartao;
-  private int idade;
+    private String idCliente, nome, cpf, endereco, numCartao, senha;
+    private int idade, cont;
+    private VetorInfinitoString inventario = new VetorInfinitoString();
+  
+    // Construtor
+    Cliente(String idCliente, String nome, String cpf, String endereco, String numCartao, int idade){
+      setIdCliente(idCliente);
+      setNome(nome);
+      setCpf(cpf);
+      setEndereco(endereco);
+      setIdade(idade);
+      setNumCartao(numCartao);
+    }
 
-  // Construtor
-  Cliente(String idCliente, String nome, String cpf, String endereco, String numCartao, int idade){
-    setIdCliente(idCliente);
-    setNome(nome);
-    setCpf(cpf);
-    setEndereco(endereco);
-    setIdade(idade);
-    setNumCartao(numCartao);
-  }
-
-  // Setters e getters
-  void setIdCliente(String idCliente){
-    this.idCliente = idCliente;
-  }
-  void setNome(String nome){
-    this.nome = nome;
-  }
-  void setCpf(String cpf){
-    this.cpf = cpf;
-  }
-  void setEndereco(String endereco){
-    this.endereco = endereco;
-  }
-  void setIdade(int idade){
-    this.idade = idade;
-  }
-  void setNumCartao(String numCartao){
-    this.numCartao = numCartao;
-  }
-  String getIdCliente(){
-    return idCliente;
-  }
-  String getNome(){
-    return nome;
-  }
-  String getCpf(){
-    return cpf;
-  }
-  String getEndereco(){
-    return endereco;
-  }
-  int getIdade(){
-    return idade;
-  }
-  String getNumCartao(){
-    return numCartao;
-  }
+    // Metodos
+    void comprar(Produto produto){
+        String novaCompra = produto.getIdProduto();
+        inventario.adicionaNoFinal(novaCompra);
+    }
+  
+    // Setters e getters
+    void setIdCliente(String idCliente){
+      this.idCliente = idCliente;
+    }
+    void setNome(String nome){
+      this.nome = nome;
+    }
+    void setCpf(String cpf){
+      this.cpf = cpf;
+    }
+    void setEndereco(String endereco){
+      this.endereco = endereco;
+    }
+    void setIdade(int idade){
+      this.idade = idade;
+    }
+    void setNumCartao(String numCartao){
+      this.numCartao = numCartao;
+    }
+    void setSenha(String senha){
+        this.senha = senha;
+    }
+    String getIdCliente(){
+      return idCliente;
+    }
+    String getNome(){
+      return nome;
+    }
+    String getCpf(){
+      return cpf;
+    }
+    String getEndereco(){
+      return endereco;
+    }
+    int getIdade(){
+      return idade;
+    }
+    String getNumCartao(){
+      return numCartao;
+    }
+    String getSenha(){
+        return senha;
+    }
 }
