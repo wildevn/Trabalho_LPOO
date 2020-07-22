3a110f24028e5936dadffa4089919609631182f1
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-22 19:03:35 -0400

    Update Pagamento.java

--- Pagamento.java
+++ Pagamento.java
@@ -1,6 +1,41 @@
-abstract class Pagamento{
-  
-  Pagamento(){
-  }
-  
+class Pagamento{
+
+    private int maxSemJuros, maxComJuros;
+    private String numBoleto;
+    private Aleatorio aleatorio = new Aleatorio();
+
+    // Construtor
+    Pagamento(){
+        numBoleto = aleatorio.geraBoleto();
+        setNumBoleto(numBoleto);
+    }
+    Pagamento(int maxSemJuros, int maxComJuros){
+        setMaxSemJuros(maxSemJuros);
+        setMaxComJuros(maxComJuros);
+    }
+
+    // Metodos
+    double calcularJuro(double preco, int juros, int parcelas){
+        juros = preco*Math.pow((1+0.05), parcelas) - preco;
+    }
+    
+    // Setters e getters
+    void setMaxSemJuros(int maxSemJuros){
+        this.maxSemJuros = maxSemJuros;
+    }
+    void setMaxComJuros(int maxComJuros){
+        this.maxComJuros = maxComJuros;
+    }
+    int getMaxSemJuros(){
+        return maxSemJuros;
+    }
+    int getMaxComJuros(){
+        return maxComJuros;
+    }
+    void setNumBoleto(String numBoleto){
+        this.numBoleto = numBoleto;
+    }
+    String getNumBoleto(){
+        return numBoleto;
+    }
 }
