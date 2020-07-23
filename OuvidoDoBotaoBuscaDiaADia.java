adccf0ede982ea24579a6bb222dfef11a37a521c
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-23 02:26:15 -0300

    Update OuvidoDoBotaoBuscaDiaADia.java

--- OuvidoDoBotaoBuscaDiaADia.java
+++ OuvidoDoBotaoBuscaDiaADia.java
@@ -23,7 +23,7 @@ public class OuvidoDoBotaoBuscaDiaADia implements ActionListener
     {
     JLabel aux;
     JPanel painelDiaADia;
-    
+    JButton botaoDetalhes;
         // encontrar e puxar todas as lojas do BD e a quantidade
 
         painelDiaADia = new JPanel(new GridLayout(20, 5)); // grid deve ser no formato (nLojas, 5 colunas)
@@ -31,16 +31,31 @@ public class OuvidoDoBotaoBuscaDiaADia implements ActionListener
         for(int i = 0; i < 100; i++)
         {
             if((i % 5) == 0)
+            {
                 aux = new JLabel("nomeProdutoDiaADia");
+                painelDiaADia.add(aux);
+            }
             else if((i % 5) == 1)
+            {
                 aux = new JLabel("");
+                painelDiaADia.add(aux);
+            }
             else if((i % 5) == 2)
+            {
                 aux = new JLabel("quantidade: x");
+                painelDiaADia.add(aux);
+            }
             else if((i % 5) == 3)
-                aux = new JLabel("");
-            else
+            {
                 aux = new JLabel("RS w.00");
-            painelDiaADia.add(aux);
+                painelDiaADia.add(aux);
+            }
+            else
+            {
+                botaoDetalhes = new JButton("Detalhes");
+                botaoDetalhes.addActionListener(new OuvidoDoBotaoDetalhes(janela, new DiaADia(" ", " asd", "fd", "TEste", "tipo 2", 23.4f, 15, "Avulso")));
+                painelDiaADia.add(botaoDetalhes);
+            }
         }
         
         janela.removePainelAnterior();
