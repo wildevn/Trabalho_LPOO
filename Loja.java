8938e903f3091f839cc5490cd3372d6f06ef9b5e
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-23 16:43:12 -0400

    Update Loja.java

--- Loja.java
+++ Loja.java
@@ -1,57 +1,77 @@
-class Loja{
+public class Loja{
 
     private String idLoja, nome, endereco, senha;
     private VetorInfinitoString idProdutos = new VetorInfinitoString();
-    private Pagamento[] formasPagamento = new Pagamento[2];
-  
+    private int maxComJuros, maxSemJuros;
+
+
     // Construtor
-    Loja(String idLoja, String nome, String endereco){
+    Loja(String idLoja, String nome, String endereco, int maxComJuros, int maxSemJuros){
       setIdLoja(idLoja);
       setNome(nome);
       setEndereco(endereco);
+      setMaxComJuros(maxComJuros);
+      setMaxSemJuros(maxSemJuros);
     }
+
+    private Pagamento cartao = new Pagamento(maxSemJuros, maxComJuros);    
     
     // Metodos
-    void cadastroProduto(Produto produto){
+    public void cadastroProduto(Produto produto){
           idProdutos.adicionaNoFinal(produto.getIdProduto());
     }
 
-    void processarCompra(String idProduto){
-        for(int i = 0; i<idProdutos.getQuantElementosAdicionados(); i++){
-            if(idProdutos.getElemento(i).equals(idProduto)){
-                // busca produto no bd com o id
-                // diminui a quantidade do produto em 1
-            }
-        }
+    public void processarCompra(Produto produto, double preco, int parcelas){
+      if(produto.getQuantidade() > 0)
+        produto.diminuiQuant();
+      else{
+        // não há o produto no estoque
+      }
+    }
+
+
+
+    public String processarCompraBoleto(){
+      Pagamento boleto = new Pagamento();
+      return boleto.getNumBoleto();
     }
   
     // Setters e getters
-    void setIdLoja(String idLoja){
+    public void setIdLoja(String idLoja){
       this.idLoja = idLoja;
     }
-    void setNome(String nome){
+    public void setNome(String nome){
       this.nome = nome;
     }
-    void setEndereco(String endereco){
+    public void setEndereco(String endereco){
       this.endereco = endereco;
     }
-    void setSenha(String senha){
-        this.senha = senha;
+    public void setSenha(String senha){
+      this.senha = senha;
     }
-    String getIdLoja(){
+    public void setMaxSemJuros(int maxSemJuros){
+      this.maxSemJuros = maxSemJuros;
+    }
+    public void setMaxComJuros(int maxComJuros){
+      this.maxComJuros = maxComJuros;
+    }
+    public int getMaxSemJuros(){
+      return maxSemJuros;
+    }
+    public int getMaxComJuros(){
+      return maxComJuros;
+    }
+    public String getIdLoja(){
       return idLoja;
     }
-    String getNome(){
+    public String getNome(){
       return nome;
     }
-    String getEndereco(){
+    public String getEndereco(){
       return endereco;
     }
-    String getSenha(){
+    public String getSenha(){
         return senha;
     }
-    VetorInfinitoString getIdProdutos()
-    {
-      return idProdutos;
-    }
+
 }
