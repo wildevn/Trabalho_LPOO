f04cd184ba3b2ce21eaffaabf8e9049f48784072
wildevn <45374307+wildevn@users.noreply.github.com>
2020-07-23 02:39:42 -0300

    Update FrameLoja.java

--- FrameLoja.java
+++ FrameLoja.java
@@ -1,10 +1,12 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 
 public class FrameLoja
@@ -19,12 +21,15 @@ private JButton botaoProdutos;
     {
         setJanela(janela);
         setLoja(loja);
-        botaoProdutos = new JButton("exibir produtos da loja");
-        painelLoja = new JPanel(new GridLayout(2, 2));
-        painelLoja.add(new JLabel(loja.getNome()));
-        painelLoja.add(new JLabel(loja.getEndereco()));
-        painelLoja.add(new JLabel("Quantidade de produtos: " + loja.idProdutos.quantElementosAdicionados())); // acho que é isso
-        painelLoja.add(botaoProdutos);
+        botaoProdutos = new JButton("Produtos da loja");
+        painelLoja = new JPanel(new GridLayout(2, 2)); // mudar a quantidade de itens para ficar correto
+        painelLoja.add(new JLabel("Nome: " + loja.getNome()));
+        painelLoja.add(new JLabel("Endereco:" + loja.getEndereco()));
+        //painelLoja.add(new JLabel("Qta max sem juros no cartao: " + loja.getMaxSemJurosCartao()));
+        //painelLoja.add(new JLabel("Qta max com juros no cartao: " + loja.getMaxComJurosCartao()));
+        painelLoja.add(new JLabel("Quantidade de produtos: " + loja.idProdutos.quantElementosAdicionados())); // falta pegar a quantidade de produtos da loja
+        botaoProdutos.addActionListener(new OuvidoDoBotaoExibirProdutosLoja(janela, loja));
+        painelLoja.add(botaoProdutos); // adicionar action listener
 
         janela.removePainelAnterior();
         janela.setPainelAnterior(painelLoja);
