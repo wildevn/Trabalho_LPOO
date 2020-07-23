edb806e4dcc7c26e0c9741a611e938d195149fbc
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-23 16:42:43 -0400

    Update Compra.java

--- Compra.java
+++ Compra.java
@@ -1,25 +1,33 @@
-class Compra{
+public class Compra{
 
-  private double valor;
-  private int parcelas;
+    private double valor;
+    private int parcelas;
 
-  // Contrutor
-  Compra(double valor, int parcelas){
+    // Contrutor
+    Compra(double valor, int parcelas){
     setValor(valor);
     setParcelas(parcelas);
-  }
+    }
 
-  // Setters e getters
-  void setValor(double valor){
+    // Metodos
+    public void gerenciaCompra(Loja loja, Produto produto, Cliente cliente){
+        loja.processarCompra(produto, valor, parcelas);
+        cliente.comprar(produto);
+    }
+
+    
+
+    // Setters e getters
+    public void setValor(double valor){
     this.valor = valor;
-  }
-  void setParcelas(int parcelas){
+    }
+    public void setParcelas(int parcelas){
     this.parcelas = parcelas;
-  }
-  double getValor(){
+    }
+    public double getValor(){
     return valor;
-  }
-  int getParcelas(){
+    }
+    public int getParcelas(){
     return parcelas;
-  }
+    }
 }
