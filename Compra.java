404f9ece777d80edb992d6e20b57f93f8a7517f1
FilipePires17 <45273732+FilipePires17@users.noreply.github.com>
2020-07-23 22:00:17 -0400

    Update Compra.java

--- Compra.java
+++ Compra.java
@@ -1,25 +1,45 @@
-class Compra{
+public class Compra{
 
-  private double valor;
-  private int parcelas;
+    private double valor, valorAtualizado;
+    private int parcelas;
 
-  // Contrutor
-  Compra(double valor, int parcelas){
+    // Contrutor
+    public Compra(double valor, int parcelas){
     setValor(valor);
     setParcelas(parcelas);
-  }
+    }
 
-  // Setters e getters
-  void setValor(double valor){
-    this.valor = valor;
-  }
-  void setParcelas(int parcelas){
-    this.parcelas = parcelas;
-  }
-  double getValor(){
-    return valor;
-  }
-  int getParcelas(){
-    return parcelas;
-  }
+    // Metodos
+    public void gerenciaCompraFinal(Loja loja, Produto produto, Cliente cliente){
+        loja.processarCompra(produto);
+        cliente.comprar(produto, valorAtualizado);
+    }
+
+    public void gerenciaCompraInicioC(Loja loja, Produto produto){
+        valorAtualizado = loja.calculaPrecoC(valor, parcelas);
+    }
+
+    public void gerenciaCompraInicioB(Loja loja, Produto produto){
+        valorAtualizado = loja.calculaPrecoB(valor);
+    }
+
+    // Setters e getters
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+    public void setParcelas(int parcelas){
+        this.parcelas = parcelas;
+    }
+    public void setValorAtualizado(double valor){
+        this.valorAtualizado = valor;
+    }
+    public double getValorAtualizado(){
+        return valorAtualizado;
+    }
+    public double getValor(){
+        return valor;
+    }
+    public int getParcelas(){
+        return parcelas;
+    }
 }
